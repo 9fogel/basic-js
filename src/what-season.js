f1aca2d21a5baa3beb405bdfde213e3cdc11e1e6
Vera Kirayonak <Vera_Kirayonak@epam.com>
2022-10-13 00:11:42 +0300

    feat: add what-season partial solution

--- src/what-season.js
+++ src/what-season.js
@@ -2,19 +2,71 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+ function Error(message) {
+  this.message = message;
 }
+
+
+function getSeason(date) {
+try {
+  if (date == undefined) {
+        return 'Unable to determine the time of year!';
+      }
+      if (date.getMonth) {
+            if (date.getMonth() === 11 || date.getMonth() === 0 || date.getMonth() === 1) {
+              return 'winter';
+            } else if (date.getMonth() === 2 || date.getMonth() === 3 || date.getMonth() === 4) {
+              return 'spring';
+            } else if (date.getMonth() === 5 || date.getMonth() === 6 || date.getMonth() === 7) {
+              return 'summer';
+            } else if (date.getMonth() === 8 || date.getMonth() === 9 || date.getMonth() === 10) {
+              return 'autumn';
+            }
+  } else {
+    throw new Error('Invalid date!');
+  }
+} catch (e) {
+  if (!date.hasOwnProperty('getMonth')) {
+        throw new Error('Invalid date!');
+      } else if (!date.hasOwnProperty('getUTCFullYear')) {
+        throw new Error('Invalid date!');
+      }
+      else {
+        throw new Error('Invalid date!');
+  }
+}
+}
+// function getSeason(date) {
+//   if (date == undefined) {
+//     return 'Unable to determine the time of year!';
+//   }
+//   if (date.getMonth) {
+//     if (date.getMonth() === 11 || date.getMonth() === 0 || date.getMonth() === 1) {
+//       return 'winter';
+//     } else if (date.getMonth() === 2 || date.getMonth() === 3 || date.getMonth() === 4) {
+//       return 'spring';
+//     } else if (date.getMonth() === 5 || date.getMonth() === 6 || date.getMonth() === 7) {
+//       return 'summer';
+//     } else if (date.getMonth() === 8 || date.getMonth() === 9 || date.getMonth() === 10) {
+//       return 'autumn';
+//     }
+//   } else if (!date.hasOwnProperty('getMonth')) {
+//     throw new Error('Invalid date!');
+//   } else {
+//     throw new Error('Invalid date!');
+//   }
+
+// }
 
 module.exports = {
   getSeason
