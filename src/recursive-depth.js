0cdc24c3fa078038a9589f98608d8718b4578319
Vera Kirayonak <Vera_Kirayonak@epam.com>
2022-10-16 20:18:09 +0300

    feat: add vigenere-cipher partial solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -12,32 +12,78 @@ const { NotImplementedError } = require('../extensions/index.js');
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
-class DepthCalculator {
+ class DepthCalculator {
+  depth = 1;
 
   calculateDepth(arr) {
 
-    this.counter = 1;
-    this.depth = 1;
-
     for (let i = 0; i < arr.length; i++) {
       if (Array.isArray(arr[i])) {
-        this.counter = 1 + this.calculateDepth(arr[i]);
+        console.log(arr[i]);
+        console.log('Array!');
+        // this.depth += 1;
+        // console.log('this.depth', this.depth);
+        arr = arr.flat();
+        // this.calculateDepth(arr);
+        this.depth = this.depth + this.calculateDepth(arr);
+        return this.depth;
+        // return (this.depth += this.calculateDepth(arr));
+      } else {
+        // console.log('else');
+        this.depth = 1;
+        continue;
       }
-    this.depth = this.counter;
     }
 
-    // if (this.counter > this.depth) {
-    //   console.log(this.counter);
-    //   this.depth = this.counter;
-    //   this.counter = 1;
-    // }
-
-    // this.counter = 1;
     console.log('depth', this.depth);
-    return this.depth;
+    let result = this.depth;
+    this.depth = 1;
+    // console.log(result);
+    return result;
 
   }
+  // depth = 1;
 }
+
+// class DepthCalculator {
+
+//   calculateDepth(arr) {
+
+//     this.counter = 1;
+//     this.depth = 1;
+
+//     for (let i = 0; i < arr.length; i++) {
+//       if (Array.isArray(arr[i])) {
+//         console.log('Array!');
+//         arr = arr[i].flat();
+//         // this.depth += this.calculateDepth(arr);
+//         // console.log(result);
+//         return this.depth += this.calculateDepth(arr);
+//       } else {
+//         return this.depth;
+//       }
+//     }
+//     console.log(this.depth);
+
+//     // for (let i = 0; i < arr.length; i++) {
+//     //   if (Array.isArray(arr[i])) {
+//     //     this.counter = 1 + this.calculateDepth(arr[i]);
+//     //   }
+//     // this.depth = this.counter;
+//     // }
+
+//     // if (this.counter > this.depth) {
+//     //   console.log(this.counter);
+//     //   this.depth = this.counter;
+//     //   this.counter = 1;
+//     // }
+
+//     // this.counter = 1;
+//     // console.log('depth', this.depth);
+//     return this.depth;
+
+//   }
+// }
 
 module.exports = {
   DepthCalculator
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,30 +3,100 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
+function Error(message) {
+  this.message = message;
+}
+
+let arrEn = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (mode = true) {
+    this.isDirect = mode;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+
+  encrypt(message, key) {
+    if(arguments.length < 2 || arguments[0] == undefined || arguments[1] == undefined) {
+      throw new Error ('Incorrect arguments!');
+    }
+
+    let upMessage = message.toUpperCase();
+    let upKey = key.toUpperCase();
+  // console.log(upKey);
+
+  let keyStr = '';
+  let k = 0;
+  let j = 0;
+  let g = 0;
+  for (let i = 0; i < upMessage.length; i++) {
+    if (upMessage[i].charCodeAt() >= 65 && upMessage[i].charCodeAt() <= 90) {
+      if (upMessage[i] === ' ') {
+      keyStr += ' ';
+      k = k;
+    }  else {
+      if (upKey[k]) {
+        keyStr += upKey[k];
+        k++;
+      } else {
+        if (upKey[j]) {
+          keyStr += upKey[j];
+          j++;
+        } else {
+          keyStr += upKey[g];
+          g++;
+        }
+      }
+    }
+    } else {
+      keyStr += upMessage[i];
+    }
+  }
+// console.log(keyStr);
+
+  let newArr = Array.from(keyStr);
+  // console.log(newArr);
+
+  newArr = newArr.map((item, index) => arrEn.indexOf(item)+arrEn.indexOf(upMessage[index]));
+
+  // console.log(newArr);
+
+  let finalStr = '';
+
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] < 0) {
+      finalStr += keyStr[i];
+    } else if (newArr[i] > 25) {
+      finalStr += arrEn[newArr[i] - 26];
+    } else {
+      finalStr += arrEn[newArr[i]];
+    }
+  }
+  // console.log(finalStr);
+  return finalStr;
+}
+
+
+  decrypt(encryptedMessage, key) {
+    if(arguments.length < 2 || arguments[0] == undefined || arguments[1] == undefined) {
+      throw new Error ('Incorrect arguments!');
+    }
   }
 }
 
